793e4f2362ecfbb293b3c31d513408e055c81993
bryce <blarkin13@gmail.com>
2024-02-27 21:11:27 -0600

    query parameters for name, carrier, tractornumber set. Chapter 11.1.5

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const { visitors } = require('./data/visitors');
 const app = express();
 
 app.get('/api/visitors', (req, res) => {
-    res.send('Test!');
+
+    let results = visitors;
+
+    if (req.query) {
+
+        results = filterByQuery(req.query, results);
+
+    }
+    res.json(results);
+    //res.send('test');
 });
+
+function filterByQuery(query, visitorsArray) {
+    let filteredResults = visitorsArray;
+    if (query.tractorNumber) {
+      filteredResults = filteredResults.filter(visitors => visitors.tractorNumber === query.tractorNumber);
+    }
+    if (query.carrier) {
+      filteredResults = filteredResults.filter(visitors => visitors.carrier === query.carrier);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(visitors => visitors.name === query.name);
+    }
+    return filteredResults;
+  }
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
